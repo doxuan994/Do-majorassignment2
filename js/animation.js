372b30df65d521449a2a88cafb9d84edb0f75149
Xuan Do <doxuancntp@gmail.com>
2019-05-02 19:02:15 -0400

    Commit.

--- js/animation.js
+++ js/animation.js
@@ -5,15 +5,11 @@
 
 window.addEventListener('scroll', function() {
     let boxes = document.querySelectorAll('.box');
-    console.log(boxes);
     for (let i = 0; i < boxes.length; i++) {
         // Relative to window.
         let boxClientY = boxes[i].getBoundingClientRect().top;
-
-        let windowInnerWidthAnimation = window.innerWidth;
-        if (windowInnerWidthAnimation >= 1200 && boxClientY < 600) {
+        if (boxClientY < 600) {
             boxes[i].classList.add("slideDown");
-
             boxes[i].classList.remove("zoomerOut");
         } else {
             boxes[i].classList.remove("slideDown");
